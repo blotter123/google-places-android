076bf4eb144589518faec24290f9edc40fa59abe
Benedikt Lotter <blotter123@googlemail.com>
2014-04-13 16:29:40 -0400

    list view with adapter

--- src/com/example/places/LocationSelectActivity.java
+++ src/com/example/places/LocationSelectActivity.java
@@ -10,34 +10,34 @@ import android.app.Activity;
 import android.content.Context;
 import android.location.Criteria;
 import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ListView;
 
 public class LocationSelectActivity extends Activity {
-	
+
 	public static final double DEFAULT_RADIUS = 1000;
 
 	ArrayList<GoogleLocation> locations;
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.location_select_activity);
-		
-		//Get Location
-		LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-		Criteria criteria = new Criteria();
-		criteria.setAccuracy(Criteria.NO_REQUIREMENT);
-		String provider = locationManager.getBestProvider(criteria, true);
 
-		// LocationManager locationManager = (LocationManager)
-		// getSystemService(Context.LOCATION_SERVICE);
-		android.location.Location location = locationManager.getLastKnownLocation(provider);
-		
-		if(location != null){
-			
+	class SetupListTask extends
+			AsyncTask<Void, Void, ArrayList<GoogleLocation>> {
+		private double latitude, longitude;
+
+		public SetupListTask(double latitude, double longitude) {
+			this.latitude = latitude;
+			this.longitude = longitude;
+		}
+
+		@Override
+		protected ArrayList<GoogleLocation> doInBackground(Void... params) {
+			ArrayList<GoogleLocation> googleLocations = new ArrayList<GoogleLocation>();
 			try {
-				this.locations = new GooglePlacesTask(location.getLatitude(),location.getLongitude(),DEFAULT_RADIUS).execute("").get();
+				googleLocations = new GooglePlacesTask(this.latitude,
+						this.longitude, DEFAULT_RADIUS).execute("").get();
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -45,9 +45,69 @@ public class LocationSelectActivity extends Activity {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			System.out.println("LocationSelectActivity.locations.length = " + this.locations.size());
+			return googleLocations;
+		};
+
+		@Override
+		protected void onPostExecute(ArrayList<GoogleLocation> googleLocations) {
+
+			GoogleLocationListAdapter adapter = new GoogleLocationListAdapter(
+					LocationSelectActivity.this,
+					R.layout.google_location_list_item, googleLocations);
+
+			final ListView listView = (ListView) findViewById(R.id.listNearbyPlaces);
+
+			listView.setAdapter(adapter);
+
+			listView.setOnItemSelectedListener(new OnItemSelectedListener() {
+
+				@Override
+				public void onItemSelected(AdapterView<?> parent, View view,
+						int position, long id) {
+					// TODO Define Action when item is selected
+
+				}
+
+				@Override
+				public void onNothingSelected(AdapterView<?> parent) {
+					// TODO Define Action when no item is selected
+
+				}
+
+			});
+		};
+	}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.location_select_activity);
+
+		// Get Location
+		LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		Criteria criteria = new Criteria();
+		criteria.setAccuracy(Criteria.NO_REQUIREMENT);
+		String provider = locationManager.getBestProvider(criteria, true);
+
+		android.location.Location location = locationManager
+				.getLastKnownLocation(provider);
+
+		if (location != null) {
+
+			try {
+				this.locations = new SetupListTask(location.getLatitude(),
+						location.getLongitude()).execute().get();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (ExecutionException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.println("LocationSelectActivity.locations.length = "
+					+ this.locations.size());
 		}
 	}
-	
 
 }
